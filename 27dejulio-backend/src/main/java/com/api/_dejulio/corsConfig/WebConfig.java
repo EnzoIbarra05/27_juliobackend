28a590b80d585f83de3ea7af581f2dbb8d9172db
Enzo Ibarra <ibarraenzo139002@gmail.com>
2025-11-17 22:45:03 -0300

    Cors final 2
    Added package declaration and necessary imports for CORS configuration.

--- 27dejulio-backend/src/main/java/com/api/_dejulio/corsConfig/WebConfig.java
+++ 27dejulio-backend/src/main/java/com/api/_dejulio/corsConfig/WebConfig.java
@@ -1,3 +1,11 @@
+package com.api._dejulio.corsConfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 @Configuration
 public class WebConfig {
 
